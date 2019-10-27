db5d5741c6cbfceea888cb08061b53bbc51a0a10
DK <davidkkimball@gmail.com>
2019-10-27 16:44:51 -0400

    Updatewithnewfeatures

---
+++ Assets/Scripts/HeadSphereControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadSphereControl : MonoBehaviour
+{
+    public Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();        
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("o"))
+        {
+            InflateSphere();      
+        }      
+        if (Input.GetKeyDown("p"))
+        {
+            CollapseSphere();   
+        }     
+    }
+    public void CollapseSphere()
+        {
+        anim.Play("HeadSphereCollapseInward"); 
+        }
+    public void InflateSphere()
+        {
+        anim.Play("HeadSphereSpreadOut"); 
+        }
+}
