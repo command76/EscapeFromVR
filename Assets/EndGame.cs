df429e5e9660555817e4343f3ed799d2652bd2c0
DK <davidkkimball@gmail.com>
2019-12-08 17:04:05 -0500

    FinalPush

--- Assets/EndGame.cs
+++ Assets/EndGame.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class EndGame : MonoBehaviour
 {
-    private float fallDelayTime = 8f;
+    private float fallDelayTime = 4f;
+    public bool endGame = false;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(FallDelay());        
+                
     }
     void Awake()
     {
@@ -18,14 +19,29 @@ public class EndGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
- 
+      /*   if (Input.GetButtonDown("VRTK_Axis9_LeftTrigger"))
+        {
+           // RetractBookShelf(); 
+        StartCoroutine(FallDelay());     
+        }   
+        if (Input.GetButtonDown("VRTK_Axis10_RightTrigger"))        
+        {
+           // RetractBookShelf(); 
+        StartCoroutine(FallDelay());     
+        }  */
     }
-
+public void restartGame()
+    {
+        if (endGame == true)
+        {                    
+         StartCoroutine(FallDelay()); 
+        }             
+    }
     IEnumerator FallDelay()
     {
         yield return new WaitForSeconds(fallDelayTime);
         SceneManager.LoadScene("gameplayscene"); 
-        
+        endGame = false;        
     }
 
 }
